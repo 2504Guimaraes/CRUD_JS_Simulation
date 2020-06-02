d8daa6f7a769281b8ef73033fb697fa5a5dbc6aa
Ivan Guimarães dos Santos da Silva <guimaraes.ivan@hotmail.com>
2020-06-02 12:08:04 -0300

    Adding again main functions file

--- js/functions/functions.js
+++ js/functions/functions.js
@@ -1,1 +1,7 @@
-'testando'
+/*
+
+    These basically are the main functions of the code, they control 
+    the main proccesses. This function list will be imported to 
+    main.js, I'll try to write them shorter as possible.
+
+*/

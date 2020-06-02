fcba0ffa91e8fa32b477c6ee34abb9cb9de51558
Ivan Guimarães dos Santos da Silva <guimaraes.ivan@hotmail.com>
2020-06-02 11:08:15 -0300

    Testing commit on new branch
    Adding an useless function

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,15 @@
-'teste'
+
+/* 
+
+    These basically are the main functions of the code, they control 
+    the main proccesses. This function list will be imported to 
+    main.js, I'll try to write them shorter as possible.
+
+*/
+
+function addUser(user) {
+    let lista = [];
+
+    lista.push(user);
+    return lista;
+}

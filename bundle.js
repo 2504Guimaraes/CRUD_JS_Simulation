7d996fca38eb5e56bf08b910ecbc24ba1b50bd42
Ivan Guimarães dos Santos da Silva <guimaraes.ivan@hotmail.com>
2020-06-05 00:31:40 -0300

    Checking commit changes

--- bundle.js
+++ bundle.js
@@ -4,10 +4,14 @@ alert('oie');
 var nome = 'Ivan Guimarães';
 var idade = 21;
 var habilidade = 'Inglês';
+var novo = 999;
+var oloko = 'bicho';
 var usuario = {
   nome: nome,
   idade: idade,
   habilidade: habilidade,
-  faculdade: 'Fatec Rubens Lara'
+  faculdade: 'Fatec Rubens Lara',
+  novo: novo,
+  oloko: oloko
 };
 console.log(usuario);
--- teste.js
+++ teste.js
@@ -3,11 +3,15 @@ alert('oie');
 const nome = 'Ivan Guimarães';
 const idade = 21;
 const habilidade = 'Inglês';
+const novo = 999;
+const oloko = 'bicho';
 
 const usuario = {
     nome,
     idade,
     habilidade,
     faculdade: 'Fatec Rubens Lara',
+    novo,
+    oloko,
 }
 console.log(usuario);
